d90f432515194c58f19ef5353732dd2522d35274
yangwenbinGit <420097009@qq.com>
2025-09-10 22:35:29 +0800

    提交修改代码

--- contracts/web3-start/33_SendingEther.sol
+++ contracts/web3-start/33_SendingEther.sol
@@ -45,7 +45,7 @@ contract SendEther {
     function sendViaCall(address payable _to) public payable {
         // Call returns a boolean value indicating success or failure.
         // This is the current recommended method to use.
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+        (bool sent, ) = _to.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
     }
 }
---
+++ contracts/web3-start/41_Import.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "./42_Foo.sol";
+
+// import {symbol1 as alias, symbol2} from "filename";
+import {Unauthorized, add as func, Point} from "./42_Foo.sol";
+
+contract Import {
+    // Initialize Foo.sol
+    Foo public foo = new Foo();
+
+    // Test Foo.sol by getting it's name.
+    function getFooName() public view returns (string memory) {
+        return foo.name();
+    }
+}
